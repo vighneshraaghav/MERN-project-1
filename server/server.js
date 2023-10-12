51f4b36d4d94a75122ed55f9384c1faf0eb3c0d8
VIGHNESH RAAGHAV <vignesherd03@gmail.com>
2023-10-12 09:39:28 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
 app.use('/',require('./routes/authRoutes'))
-
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials: true
+}))
 mongoose
   .connect(
     process.env.MONGO_URI
