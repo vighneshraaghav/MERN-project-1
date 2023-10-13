56409c47193ec0a95d2ca695ca4bb315a28cc58d
vighneshraaghav <114808142+vighneshraaghav@users.noreply.github.com>
2023-10-13 21:34:04 +0530

    Update userContext.js

--- client/src/context/userContext.js
+++ client/src/context/userContext.js
@@ -1,26 +1,28 @@
 import axios from 'axios';
-import {createContext, useState, useEffect} from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-export const UserContext = createContext({})
 
-export function UserContextProvider({children}){
+export const UserContext = createContext({});
+
+export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  useEffect((user)=>{
-    if(isLoggedIn){
-      if(!user){
-        axios.get('/profile').then(({data})=>{
-          setUser(data)
-        })
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      if (!user) {
+        axios.get('/profile').then(({ data }) => {
+          setUser(data);
+        });
       }
+    } else {
+      setUser(null);
     }
-    else{
-      setUser(null)
-    }
-  },[isLoggedIn])
-  return(
-    <UserContext.Provider value={{user, setUser}}>
+  }, [isLoggedIn, user]); // Include 'user' as a dependency
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
-  )
+  );
 }
