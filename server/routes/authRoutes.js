dd2bf50878b1aec21e9727b82cb276996aa73b09
vighneshraaghav <114808142+vighneshraaghav@users.noreply.github.com>
2023-10-14 23:35:55 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const session = require("express-session");
-const RedisStore = require('connect-redis')(session);
+const RedisStore = require('connect-redis').default;
 const { createClient } = require('redis');
 const cors = require("cors");
 const {
